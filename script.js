29504eee946966a55105799524b1822033c3dd94
mina <rlfalsk0301@gmail.com>
2023-01-01 20:31:51 +0900

    스크롤이벤트 수정

--- script.js
+++ script.js
@@ -1,69 +1,34 @@
-var UIAnimation = function(props){
-  //필드
-  var __target = document.querySelector(props['target']);
-  console.log(__target)
-  var winScrollTop;
-  var sectionOfffsetTop;
-  var sectionHeight;
-  var sectionOffsetBottom;
-  var checkInSection = false;
-  var fastIn;
+let aosElements = Array.from(document.querySelectorAll('.aos'));
 
-  // var startFunction = props['option'];
-  // var isFunction = typeof(startFunction) === 'function' ? true : false;
+window.addEventListener('scroll', throttle(scanElement, 500));
 
-  function init(){
-    
-    setScroll();
-    setInSection();
-  }
-  
-  function setProperty(){
-    fastIn = window.innerHeight / 2;
-    winScrollTop = window.pageYOffset;
-    sectionOfffsetTop = __target.getBoundingClientRect().top + winScrollTop - fastIn;
-    sectionHeight = __target.offsetHeight;
-    sectionOffsetBottom = sectionOfffsetTop + sectionHeight + fastIn;
-    
-  }
-
-  function setInSection(){
-    setProperty();
-
-    if(winScrollTop >= sectionOfffsetTop && winScrollTop <= sectionOffsetBottom) {
-      if(!checkInSection) {
-        checkInSection = true;
-        __target.classList.add("active")
-        // startFunction();
+function scanElement(e){
+  console.count(e);
+  aosElements.forEach(element => {
+    if(isVisible(element)){
+      element.classList.add('active');
+    }else{
+      element.classList.remove('active');
     }
-    }
-  }
-
-  function setScroll(){
-    window.addEventListener('scroll', function(){
-      setInSection();
-    }, false);
-  }
-
-  init();
-}
-
-//구현(실체)클래스
-var ScrollAnimation = function(element){
-  var result = new UIAnimation({
-    "target" : element
   });
 }
 
+function isVisible(element){
+  const elementDiv = element.getBoundingClientRect();
+  let distanceFromTop = -300;
+  return elementDiv.top - window.innerHeight < distanceFromTop ? true : false;
+}
 
-//실행클래스
-var result1 = new ScrollAnimation(".sec01");
-var result2 = new ScrollAnimation(".section_product .prd_mask");
-
-// ScrollAnimation(".sec01", function(){
-	// 	document.querySelector('.sec01').classList.add('active');
-	// });
-	
-	// ScrollAnimation('.section_product .prd_mask', function(){
-	// 	document.querySelector('.section_product .prd_mask').classList.add('active');
-	// });
+function throttle(fn, delay){
+  let lastCall = 0;
+  return function(arguments){
+    console.log(arguments)
+    let context = this;
+    let current = new Date().getTime();
+    if(current - lastCall < delay){
+      return;
+    }
+    lastCall = current;
+    return fn.apply(context, arguments)
+  }
+}
---
+++ script2.js
@@ -0,0 +1,56 @@
+
+UIAnimation = function(props){
+
+  let instanceVar = {
+    target : props['target'],
+    transYAniSections : props['target'].querySelectorAll('.trans-y-ani'),
+  }
+
+  this.options = Object.assign({}, instanceVar, props);
+
+  window.addEventListener('load', () => {
+    this.smoothScroll();
+  });
+  window.addEventListener('resize', () => {
+    this.smoothScroll();
+  });
+  window.addEventListener('scroll', () => {
+    this.throttle(this.smoothScroll, 1000)
+  })
+}
+UIAnimation.prototype.smoothScroll = function(){}
+UIAnimation.prototype.isVisibleSection = function(){}
+UIAnimation.prototype.throttle = function(fn, delay){
+  let lastCall = 0;
+  let current = new Date().getTime();
+  if(current - lastCall < delay){
+    return;
+  }
+  lastCall = current;
+  
+  return fn.apply(this, arguments);
+  
+}
+
+TranslateYAni = function(){}
+TranslateYAni.prototype = new UIAnimation({
+  'target' : document.querySelector('[data-module="transAnimation"]'),
+});
+TranslateYAni.prototype.constructor = TranslateYAni;
+TranslateYAni.prototype.smoothScroll = function(){
+  Array.prototype.forEach.call(this.options.transYAniSections, (element) =>{
+    if(this.isVisibleSection(element)){
+      element.classList.add('is-trans-y-ani');
+    }
+    return true;
+  });
+  
+}
+TranslateYAni.prototype.isVisibleSection = function(element){
+  const elementDiv = element.getBoundingClientRect();
+  let distanceFromTop = -100;
+  return elementDiv.top - window.innerHeight < distanceFromTop ? true : false;
+}
+
+var animation = new TranslateYAni();
+
