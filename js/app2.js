919c2dc6457718a476e2b87e66a76f8c70045947
mina <rlfalsk0301@gmail.com>
2023-01-18 21:37:21 +0900

    의사클래스 상속 slider 작업

--- js/app2.js
+++ js/app2.js
@@ -1,109 +1,125 @@
 const Slider = (function(){
-  const elements = document.querySelectorAll('#main_feature .feature-cgroup .item-track .item');
 
-  let itemContainer = null;
+  function Slider(){
+    this.idx = 0;
+
+    return function(){
+      this.setEventButton();
+    }
+    
+  }
+  
+  Slider.prototype.slideItem = function(){
+
+  }
+
+  Slider.prototype.nextSlide = function() {
+    this.slideItem();
+  }
+
+  Slider.prototype.prevSlide = function() {
+    this.slideItem();
+  }
+
+  Slider.prototype.setEventButton = function(props){
+    const prevBtn = props.prev;
+    const nextBtn = props.next;
+
+    nextBtn.addEventListener('click', () => {
+      this.idx++;
+      this.nextSlide();
+    });
+
+    prevBtn.addEventListener('click', () => {
+      this.idx--;
+      this.prevSlide();
+    });
+  }
+
+  return Slider;
+  
+}());
+
+const MainBannerSlide = (function(){
   let itemTrack = null;
   let itemWidth = null;
-  let nextBtn = null;
-  let prevBtn = null;
+  let item = null;
+  let isSliding = false;
 
-  const itemsInfo = {
-    offset : 0,
-    position : {
-      current: 0,
-      min : 0,
-      max : elements.length - 1
-    }
-  }
-
-  function Slider(props){
-    this.isSliding = false;
-    itemContainer = props.container;
+  function MainBannerSlide(props){
     itemTrack = props.itemTrack;
     itemWidth = props.itemWidth.offsetWidth + 40;
+    item = itemTrack.children;
 
-    init();
+    this.setInit();
+    this.createCloneNode();
+    
+    Slider.apply(this, arguments);
+
   }
 
-  function init(){
-    setInit();
-    createClone();
-  }
+  MainBannerSlide.prototype = Object.create(Slider.prototype);
+  MainBannerSlide.prototype.constructor = MainBannerSlide;
 
-  function setInit(){
-    itemTrack.style.transform = 'translateX(0)';
+  MainBannerSlide.prototype.setInit = function(){
+    itemTrack.style.transform = `translateX(0)`;
     const items = Array.from(itemTrack.children).filter(child => {
-      return (
-        !child.classList.contains('clone')
-      )
+      return !child.classList.contains('clone');
     });
     itemTrack.style.left = `${itemWidth * items.length * -1}px`;
   }
 
-  function createClone(){
+  MainBannerSlide.prototype.createCloneNode = function() {
     const nodes = Array.from(itemTrack.children);
-    nodes.forEach((node) => {
+    nodes.forEach(node => {
       const clone = node.cloneNode(true);
       clone.classList.add('clone');
       itemTrack.append(clone);
     });
-    nodes.reverse().forEach((node) => {
+    nodes.reverse().forEach(node => {
       const clone = node.cloneNode(true);
       clone.classList.add('clone');
       itemTrack.prepend(clone);
     });
   }
 
-  function slide(){
+  MainBannerSlide.prototype.slideItem = function(){
+    // if(isSliding){
+    //   return;
+    // }
+    // isSliding = true;
+    // setTimeout(() => { isSliding = false; }, 1000);
+
     itemTrack.style.transition = '1s';
-    itemTrack.style.transform = `translateX(${itemWidth * itemsInfo.position.current * -1}px)`;
+    itemTrack.style.transform = `translateX(${itemWidth * this.idx * -1}px)`;
 
-    const orgLength = itemTrack.children.length / 3;
+    const orgLength = item.length / 3;
 
-    console.log(itemsInfo.position.current)
-
-    if(orgLength <= itemsInfo.position.current){
-      itemsInfo.position.current = 0;
+    if(orgLength <= this.idx) {
+      this.idx = 0;
     }
+
+    if(this.idx <= -3) {
+      this.idx = 2;
+    }
+    console.log(this.idx)
 
     itemTrack.addEventListener('transitionend', () => {
       itemTrack.style.transition = '0s';
-      itemTrack.style.transform = `translateX(${itemWidth * itemsInfo.position.current * -1}px)`;
+      itemTrack.style.transform = `translateX(-${itemWidth * this.idx}px)`;
     });
   }
 
-  function nextSlide(){
-    
-  }
-
-  function updateItemInfo(){
-    
-    itemsInfo.position.current = itemsInfo.position.current + 1
-    slide();
-  }
-
-  Slider.prototype.setEventButton = function(props){
-    nextBtn = props.next;
-    prevBtn = props.prev;
-
-    nextBtn.addEventListener('click', () => {
-      updateItemInfo();
-    });
-
-    prevBtn.addEventListener('click', () => {
-      console.log('prev');
-    });
-  }
-
-  return Slider;
+  return MainBannerSlide;
 }());
 
-const slide1 = new Slider({
+const mainBannerSlide = new MainBannerSlide({
   container : document.querySelector('#main_feature .feature-cgroup[data-menu="nav1"] .item-container'),
   itemTrack : document.querySelector('#main_feature .feature-cgroup[data-menu="nav1"] .item-track'),
   itemWidth : document.querySelector('#main_feature .feature-cgroup[data-menu="nav1"] .item-track .item'),
 });
-slide1.setEventButton({
+
+mainBannerSlide.setEventButton({
   prev : document.querySelector('#main_feature .feature-cgroup[data-menu="nav1"] .left .btn-group button.prev'),
   next : document.querySelector('#main_feature .feature-cgroup[data-menu="nav1"] .left .btn-group button.next')
 });
